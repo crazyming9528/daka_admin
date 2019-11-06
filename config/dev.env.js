a03169aaf9376ae3125df07fee6e60ce9cabec7b
crazyming <crazyming9528@qq.com>
2019-11-06 21:48:33 +0800

    init

---
+++ config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"http://120.78.149.155:8080"'
+})
